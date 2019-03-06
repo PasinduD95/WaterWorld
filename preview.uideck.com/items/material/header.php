c3f00d4aea724a46eaf2e057d3993a8d05c37103
PasinduD95 <pasindudilshan30@gmail.com>
2019-03-06 18:20:03 +0530

    first page

---
+++ preview.uideck.com/items/material/header.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from preview.uideck.com/items/material/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 22 Feb 2019 06:08:15 GMT -->
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Material - Material Design Bootstrap 4 Template</title>
+
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="assets/css/materialdesignicons.min.css">
+
+<link rel="stylesheet" href="assets/css/material.min.css">
+
+<link rel="stylesheet" href="assets/css/ripples.min.css">
+
+<link rel="stylesheet" href="assets/css/owl.carousel.css">
+
+<link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+<link rel="stylesheet" href="assets/css/slicknav.css">
+
+<link rel="stylesheet" href="assets/css/animate.css">
+
+<link rel="stylesheet" href="assets/css/style.css">
+
+<link rel="stylesheet" href="assets/css/responsive.css">
+
+<link rel="stylesheet" href="assets/css/color-switcher.css">
+
+<link rel="stylesheet" type="text/css" href="assets/css/colors/indigo.css" media="screen" />
+</head>
+<body>
+
+<header id="header">
+<nav class="navbar navbar-expand-lg fixed-top scrolling-navbar nav-bg">
+<div class="container">
+
+<div class="navbar-header">
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar" aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
+<span class="sr-only">Toggle navigation</span>
+<span class="mdi mdi-menu"></span>
+<span class="mdi mdi-menu"></span>
+<span class="mdi mdi-menu"></span>
+</button>
+<a class="navbar-brand" href="index-2.html"><img src="assets/images/images.jpg" alt=""></a>
+</div>
+<div class="collapse navbar-collapse" id="main-navbar">
+<ul class="navbar-nav mr-auto w-100 justify-content-end">
+<li class="nav-item dropdown active">
+<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+Home
+</a>
+<div class="dropdown-menu">
+<a class="dropdown-item active" href="index-2.html">Home V1</a>
+<a class="dropdown-item" href="index-3.html">Home V2</a>
+</div>
+</li>
+<li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+Pages
+</a>
+<div class="dropdown-menu">
+<a class="dropdown-item" href="about-us.html">About Us 1</a>
+<a class="dropdown-item" href="about-us2.html">About Us 2</a>
+<a class="dropdown-item" href="team.html">Team Members</a>
+<a class="dropdown-item" href="services.html">Services</a>
+<a class="dropdown-item" href="contact-us.html">Contact Us 1</a>
+<a class="dropdown-item" href="contact-us2.html">Contact Us 2</a>
+<a class="dropdown-item" href="404.html">404</a>
+</div>
+</li>
+<li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+Components
+</a>
+<div class="dropdown-menu">
+<a class="dropdown-item" href="tab.html">Tabs</a>
+<a class="dropdown-item" href="alert.html">Alert</a>
+<a class="dropdown-item" href="accordion.html">Accordions</a>
+<a class="dropdown-item" href="pricing.html">Pricing Tables</a>
+<a class="dropdown-item" href="buttons.html">Buttons</a>
+<a class="dropdown-item" href="icons.html">Icons</a>
+<a class="dropdown-item" href="carousel.html">Carousel</a>
+<a class="dropdown-item" href="counter.html">Counter</a>
+<a class="dropdown-item" href="map.html">Google Map</a>
+</div>
+</li>
+<li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+Portfolio
+</a>
+<div class="dropdown-menu">
+<a class="dropdown-item" href="portfolio-2.html">Portfolio 2 columns</a>
+<a class="dropdown-item" href="portfolio.html">Portfolio 3 columns</a>
+<a class="dropdown-item" href="portfolio-single.html">Portfolio Single</a>
+</div>
+</li>
+<li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+Blog
+</a>
+<div class="dropdown-menu">
+<a class="dropdown-item" href="blog.html">Blog Page</a>
+<a class="dropdown-item" href="blog-single.html">Blog Single Page</a>
+</div>
+</li>
+<li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+Contact Us
+</a>
+<div class="dropdown-menu">
+<a class="dropdown-item" href="contact-us.html">Contact Us 1</a>
+<a class="dropdown-item" href="contact-us2.html">Contact Us 2</a>
+</div>
+</li>
+</ul>
+
+<div class="search-icon">
+<span class="open-search">
+<i class="mdi mdi-magnify btn btn-common"></i>
+</span>
+</div>
+<form role="search" class="navbar-form">
+<div class="container">
+<div class="row">
+<div class="form-group has-feedback">
+<input type="text" placeholder="Type and search ..." class="form-control">
+<div class="close"><i class="mdi mdi-close"></i></div>
+</div>
+</div>
+</div>
+</form>
+
+</div>
+</div>
+
+<ul class="wpb-mobile-menu">
+<li>
+<a class="active" href="#">
+Home
+</a>
+<ul class="dropdown">
+<li>
+<a class="active" href="index-2.html">Home v1</a>
+</li>
+<li>
+<a href="index-3.html">Home v2</a>
+</li>
+</ul>
+</li>
+<li>
+<a href="#">
+Pages
+</a>
+<ul class="dropdown">
+<li>
+<a href="about-us.html">About Us 1</a>
+</li>
+<li>
+<a href="about-us2.html">About Us 2</a>
+</li>
+<li>
+<a href="team.html">Team Members</a>
+</li>
+<li>
+<a href="services.html">Services</a>
+</li>
+<li>
+<a href="contact-us.html">Contact Us 1</a>
+</li>
+<li>
+<a href="contact-us2.html">Contact Us 2</a>
+</li>
+<li>
+<a href="404.html">404</a>
+</li>
+</ul>
+</li>
+<li>
+<a href="#">
+Elements
+</a>
+<ul class="dropdown">
+<li>
+<a href="tab.html">Tabs</a>
+</li>
+<li>
+<a href="alert.html">Alert</a>
+</li>
+<li>
+<a href="accordion.html">Accordions</a>
+</li>
+<li>
+<a href="pricing.html">Pricing Tables</a>
+</li>
+<li>
+<a href="buttons.html">Buttons</a>
+</li>
+<li>
+<a href="icons.html">Icons</a>
+</li>
+<li>
+<a href="carousel.html">Carousel</a>
+</li>
+<li>
+<a href="counter.html">Counter</a>
+</li>
+<li>
+<a href="map.html">Google Map</a>
+</li>
+</ul>
+</li>
+<li>
+<a href="#">
+Portfolio
+</a>
+<ul class="dropdown">
+<li>
+<a href="portfolio-2.html">Portfolio 2 columns</a>
+</li>
+<li>
+<a href="portfolio.html">Portfolio 3 columns</a>
+</li>
+<li>
+<a href="portfolio-single.html">Portfolio Single</a>
+</li>
+</ul>
+</li>
+<li>
+<a href="about.html">
+Blog
+</a>
+<ul class="dropdown">
+<li>
+<a href="blog.html">Blog Page</a>
+</li>
+<li>
+<a href="blog-single.html">Blog Single Page</a>
+</li>
+</ul>
+</li>
+<li>
+<a href="#">
+Contact Us
+</a>
+<ul class="dropdown">
+<li>
+<a href="contact-us.html">Contact Us 1</a>
+</li>
+<li>
+<a href="contact-us2.html">Contact Us 2</a>
+</li>
+</ul>
+</li>
+</ul>
+
+</nav>
+</header>
+</body>
+</html>
